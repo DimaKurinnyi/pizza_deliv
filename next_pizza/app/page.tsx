29d0f6ab4ce756a634241b3e7c2da8fc14c0ffc3
Dima <dimakurinnoy8@gmail.com>
2024-11-18 22:54:57 +0100

    test

--- next_pizza/app/page.tsx
+++ next_pizza/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <h1>home 
+  return <h1>homeee 
     <Button variant={"outline"}>Shop</Button>
   </h1>;
 }
